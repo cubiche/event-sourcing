d48252cf402104c18900807fff88c928953c1536
Ivannis <ivannis.suarez@gmail.com>
2016-06-28 15:54:55 +0000

    added event-sourcing as a separate component

---
+++ DomainEvent.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * This file is part of the Cubiche package.
+ *
+ * Copyright (c) Cubiche
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Cubiche\Domain\EventSourcing;
+
+use Cubiche\Domain\EventPublisher\DomainEvent as BaseDomainEvent;
+use Cubiche\Domain\Model\IdInterface;
+
+/**
+ * DomainEvent class.
+ *
+ * @author Ivannis Suárez Jerez <ivannis.suarez@gmail.com>
+ */
+class DomainEvent extends BaseDomainEvent implements DomainEventInterface
+{
+    /**
+     * @var IdInterface
+     */
+    protected $aggregateId;
+
+    /**
+     * @var int
+     */
+    protected $version;
+
+    /**
+     * EntityDomainEvent constructor.
+     *
+     * @param IdInterface $aggregateId
+     */
+    public function __construct(IdInterface $aggregateId)
+    {
+        parent::__construct();
+
+        $this->aggregateId = $aggregateId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function aggregateId()
+    {
+        return $this->aggregateId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function version()
+    {
+        return $this->version;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+    }
+}
---
+++ EventStore/EventStream.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * This file is part of the Cubiche package.
+ *
+ * Copyright (c) Cubiche
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Cubiche\Domain\EventSourcing\EventStore;
+
+use Cubiche\Domain\EventSourcing\DomainEventInterface;
+use Cubiche\Domain\Model\IdInterface;
+
+/**
+ * EventStream class.
+ *
+ * @author Ivannis Suárez Jerez <ivannis.suarez@gmail.com>
+ */
+class EventStream
+{
+    /**
+     * @var string
+     */
+    protected $streamName;
+
+    /**
+     * @var IdInterface
+     */
+    protected $aggregateId;
+
+    /**
+     * @var DomainEventInterface[]
+     */
+    protected $events = [];
+
+    /**
+     * EntityDomainEvent constructor.
+     *
+     * @param string                 $streamName
+     * @param IdInterface            $aggregateId
+     * @param DomainEventInterface[] $events
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($streamName, IdInterface $aggregateId, array $events)
+    {
+        $this->streamName = $streamName;
+        $this->aggregateId = $aggregateId;
+
+        foreach ($events as $event) {
+            if (!$event instanceof DomainEventInterface) {
+                throw new \InvalidArgumentException(
+                    sprintf(
+                        'The event must be an instance of %s. Instance of %s given',
+                        DomainEventInterface::class,
+                        is_object($event) ? get_class($event) : gettype($event)
+                    )
+                );
+            }
+
+            $this->addEvent($event);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function streamName()
+    {
+        return $this->streamName;
+    }
+
+    /**
+     * @return IdInterface
+     */
+    public function aggregateId()
+    {
+        return $this->aggregateId;
+    }
+
+    /**
+     * @return DomainEventInterface[]
+     */
+    public function events()
+    {
+        return $this->events;
+    }
+
+    /**
+     * @param DomainEventInterface $event
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function addEvent(DomainEventInterface $event)
+    {
+        if (!$event->aggregateId()->equals($this->aggregateId)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Event-stream can only contain events for identifier %s, but got %s',
+                    $this->aggregateId->toNative(),
+                    $event->aggregateId()->toNative()
+                )
+            );
+        }
+
+        $this->events[] = $event;
+    }
+}
